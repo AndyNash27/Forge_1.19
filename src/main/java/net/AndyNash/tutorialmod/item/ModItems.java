6ba4410dc167d978e9dfac814cfd94b2eb6933f5
AndyNash27 <102205653+AndyNash27@users.noreply.github.com>
2023-05-07 11:53:31 +0800

    Added "recipes - Amethyst_block"       "Eight_Ball"

--- src/main/java/net/AndyNash/tutorialmod/item/ModItems.java
+++ src/main/java/net/AndyNash/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.AndyNash.tutorialmod.item;
 
 import net.AndyNash.tutorialmod.TutorialMod;
+import net.AndyNash.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,6 +16,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> AMETHYST = ITEMS.register("amethyst",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
