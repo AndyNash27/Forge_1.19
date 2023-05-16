fafbe545a3fbffe9e1da63b2db498c83961ca509
AndyNash27 <102205653+AndyNash27@users.noreply.github.com>
2023-05-17 01:38:25 +0800

    Added "jumpy_block"       "introduction of Eight_Ball"

--- src/main/java/net/AndyNash/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/AndyNash/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.AndyNash.tutorialmod.block;
 
 import net.AndyNash.tutorialmod.TutorialMod;
+import net.AndyNash.tutorialmod.block.custom.JumpyBlock;
 import net.AndyNash.tutorialmod.item.ModCreativeModeTab;
 import net.AndyNash.tutorialmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
@@ -23,6 +24,10 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> AMETHYST_BLOCK = registerBlock("amethyst_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
+            () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
